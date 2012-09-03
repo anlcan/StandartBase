d802e5bacef8f32b1c7f79eab42bd0f8884beefc
anlcan <anilcanbaykal@gmail.com>
2012-09-04 01:25:43 +0300

    Initial Commit

---
+++ StandartBase/StandartBase.h
@@ -0,0 +1,13 @@
+//
+//  StandartBase.h
+//  StandartBase
+//
+//  Created by Anil Can Baykal on 9/4/12.
+//  Copyright (c) 2012 Anil Can Baykal. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface StandartBase : NSObject
+
+@end
---
+++ StandartBase/StandartBase.m
@@ -0,0 +1,13 @@
+//
+//  StandartBase.m
+//  StandartBase
+//
+//  Created by Anil Can Baykal on 9/4/12.
+//  Copyright (c) 2012 Anil Can Baykal. All rights reserved.
+//
+
+#import "StandartBase.h"
+
+@implementation StandartBase
+
+@end
