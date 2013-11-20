cb6440cd706207184043c27aabb4503e4a2d43fa
anilcanbaykal@gmail.com <anilcanbaykal@gmail.com>
2013-11-20 11:54:43 +0200

    arc ready

---
+++ StandartBase/StandartUtil.h
@@ -0,0 +1,13 @@
+//
+//  StandartUtil.h
+//  Skynet
+//
+//  Created by Anil Can Baykal on 11/20/13.
+//  Copyright (c) 2013 Anil Can Baykal. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface StandartUtil : NSObject
+
+@end
---
+++ StandartBase/StandartUtil.m
@@ -0,0 +1,13 @@
+//
+//  StandartUtil.m
+//  Skynet
+//
+//  Created by Anil Can Baykal on 11/20/13.
+//  Copyright (c) 2013 Anil Can Baykal. All rights reserved.
+//
+
+#import "StandartUtil.h"
+
+@implementation StandartUtil
+
+@end
